9ae6ef87180fed546d48dda981c6e8b3162df779
Moringa School <abdallah.abdul@student.moringaschool.com>
2021-05-24 04:41:36 +0300

    add the initial commits

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {ServicesService} from '../services.service'
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+ 
+  service:any;
+  getServices: any;
+
+  constructor(private findServices:ServicesService) { 
+    this.findServices.getServiceData().subscribe((service: any) =>{
+      console.log(service);
+      this.service = service;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/services.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+
+  private username : string;
+  private clientId ='3a89a5fbf7242a533ac4';
+  private clientSecret ='ddee8cbd76aba1768e973bcab4e73be1c7e03fa8';
+  constructor(private http:HttpClient) {
+    this.username = "Habib001-coder";
+  }
+  getServiceData(){
+    return this.http.get("https://api.github.com/users/Habib001-coder")
+  }
+}
